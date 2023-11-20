792e94298e988f29671a3aba7785ddca36331c97
Gathrean Dela Cruz <gathrean@icloud.com>
2023-11-19 22:14:11 -0800

    Added REPLACE and DELETE in my UserDao

--- app/src/main/java/com/example/lab9gathreandelacruz/data/UserDao.kt
+++ app/src/main/java/com/example/lab9gathreandelacruz/data/UserDao.kt
@@ -1,7 +1,9 @@
 package com.example.lab9gathreandelacruz.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 // DAO = data access object
@@ -13,6 +15,9 @@ interface UserDao {
     @Query("SELECT * FROM users_table")
     fun getAll(): List<LocalUser>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // replace the user with the new one if it already exists
     fun insert(user: LocalUser)
+
+    @Delete
+    fun delete(user: LocalUser) // delete a specific user
 }
